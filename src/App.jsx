dd3d8a2e30b5852addcf4818287e9a7a710250d7
devSeyf <ssaaiiffjjaammaall@gmail.com>
2025-12-20 13:58:04 +0300

    handling reouters

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,19 @@
 
 import { Routes, Route } from 'react-router-dom';
 import './App.css'
-import Navbar from "./components/Navbar"   
-import Slider from "./components/Slider"   
-import ProductsList from "./components/ProductsList"   
-import About from "./components/About"   
+
+
+import About from "./components/About"
+import Home from "./components/Home"
 
 function App() {
   return (
     <>
       <div className="App">
-        <Navbar />
-
-       
-          <Routes>
-            <Route
-              path="/"
-              element={
-                <>
-                  <Slider />
-                  <ProductsList />
-                </>
-              }
-            />
-            <Route path="/about" element={<About />} />
-          </Routes>
-        
-
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
       </div>
     </>
   )
---
+++ src/components/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "./Navbar"   
+import Slider from "./Slider"   
+import ProductsList from "./ProductsList"  
+export default function Home() {
+return (
+    <>
+      <Navbar />
+<Slider /> 
+  <ProductsList />
+    </>
+)
+}
