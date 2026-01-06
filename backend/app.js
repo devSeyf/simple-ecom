f645ada23749f85c4ff86baffd5712db26f9987b
devSeyf <ssaaiiffjjaammaall@gmail.com>
2026-01-07 00:37:14 +0300

    http 2

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,13 @@
+const http = require('http');
 
-
-const http = require('http')
-
-const server =http.createServer((req, res) => {
+const server = http.createServer((req, res) => {
+  // Get the URL and HTTP method
+  const { url, method } = req;
+  
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hi !\n');
-})
+  res.end(`You made a ${method} request to ${url}`);
+});
 
-server.listen(3000, 'localhost', () => {
-  console.log(`Server running at http://localhost:3000/`);
+server.listen(3000, () => {
+  console.log('Server running at http://localhost:3000/');
 });
